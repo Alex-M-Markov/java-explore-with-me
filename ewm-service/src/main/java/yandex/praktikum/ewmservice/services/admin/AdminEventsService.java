2346a717643ab906ec9f86c2b4875a2fa4dfab85
Alex-M-Markov <markov.braun@yandex.ru>
2022-12-31 01:02:54 +0300

    refactor: perform all changes recommended in the fifth review

--- ewm-service/src/main/java/yandex/praktikum/ewmservice/services/admin/AdminEventsService.java
+++ ewm-service/src/main/java/yandex/praktikum/ewmservice/services/admin/AdminEventsService.java
@@ -11,6 +11,8 @@ import yandex.praktikum.ewmservice.entities.State;
 import yandex.praktikum.ewmservice.entities.dto.event.AdminUpdateEventRequest;
 import yandex.praktikum.ewmservice.entities.dto.event.EventFullDto;
 import yandex.praktikum.ewmservice.entities.dto.statistics.EndpointHitDto;
+import yandex.praktikum.ewmservice.entities.dto.statistics.StatsRequestDto;
+import yandex.praktikum.ewmservice.entities.dto.statistics.ViewStatsDto;
 import yandex.praktikum.ewmservice.entities.mappers.EventsMapper;
 import yandex.praktikum.ewmservice.exceptions.*;
 import yandex.praktikum.ewmservice.repositories.CategoriesRepository;
@@ -20,6 +22,8 @@ import yandex.praktikum.ewmservice.services.privateuser.PrivateRequestsService;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -87,12 +91,35 @@ public class AdminEventsService {
         ));
         List<Event> events = eventsRepository.getEventsAdmin(users, states, categories, rangeStart, rangeEnd, from,
                 size);
+        Map<String, Event> eventsWithUris = new HashMap<>();
+        for (Event event : events) {
+            eventsWithUris.put("/events/" + event.getId(), event);
+        }
+        StatsRequestDto statsRequestDto = StatsRequestDto.builder()
+                .withStart(LocalDateTime.MIN)
+                .withEnd(LocalDateTime.now())
+                .withUris(new ArrayList<>(eventsWithUris.keySet()))
+                .withUnique(false)
+                .build();
+        List<ViewStatsDto> stats = statsWebClient.getViews(statsRequestDto);
+        Map<Long, Long> eventsViewsStats = new HashMap<>();
+        for (ViewStatsDto viewStatsDto : stats) {
+            if (viewStatsDto.getUri() != null) {
+                eventsViewsStats.put(eventsWithUris.get(viewStatsDto.getUri()).getId(), viewStatsDto.getHits());
+            }
+        }
         List<ParticipationRequest> allRequestsOfEvents = requestsRepository.findParticipationRequestsByEventIn(events);
         Map<Event, Long> collect = allRequestsOfEvents.stream()
                 .collect(Collectors.groupingBy(ParticipationRequest::getEvent, Collectors.counting()));
-        return events.stream()
+        List<EventFullDto> eventFullDtos = events.stream()
                 .map((e) -> EventsMapper.eventToFullDto(e, collect.get(e)))
                 .collect(Collectors.toList());
+        if (eventsViewsStats.size() > 0) {
+            for (EventFullDto eventFullDto : eventFullDtos) {
+                eventFullDto.setViews(eventsViewsStats.get(eventFullDto.getId()));
+            }
+        }
+        return eventFullDtos;
     }
 
     @Transactional
--- ewm-service/src/main/java/yandex/praktikum/ewmservice/services/privateuser/PrivateEventsService.java
+++ ewm-service/src/main/java/yandex/praktikum/ewmservice/services/privateuser/PrivateEventsService.java
@@ -12,6 +12,8 @@ import yandex.praktikum.ewmservice.entities.dto.event.EventShortDto;
 import yandex.praktikum.ewmservice.entities.dto.event.NewEventDto;
 import yandex.praktikum.ewmservice.entities.dto.event.UpdateEventRequest;
 import yandex.praktikum.ewmservice.entities.dto.statistics.EndpointHitDto;
+import yandex.praktikum.ewmservice.entities.dto.statistics.StatsRequestDto;
+import yandex.praktikum.ewmservice.entities.dto.statistics.ViewStatsDto;
 import yandex.praktikum.ewmservice.entities.mappers.EventsMapper;
 import yandex.praktikum.ewmservice.exceptions.*;
 import yandex.praktikum.ewmservice.repositories.CategoriesRepository;
@@ -21,9 +23,7 @@ import yandex.praktikum.ewmservice.repositories.UsersRepository;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -127,12 +127,35 @@ public class PrivateEventsService {
                 LocalDateTime.now()
         ));
         List<Event> events = eventsRepository.findEventsByInitiator_Id(userId, PageRequest.of(from, size));
+        Map<String, Event> eventsWithUris = new HashMap<>();
+        for (Event event : events) {
+            eventsWithUris.put("/events/" + event.getId(), event);
+        }
+        StatsRequestDto statsRequestDto = StatsRequestDto.builder()
+                .withStart(LocalDateTime.MIN)
+                .withEnd(LocalDateTime.now())
+                .withUris(new ArrayList<>(eventsWithUris.keySet()))
+                .withUnique(false)
+                .build();
+        List<ViewStatsDto> stats = statsWebClient.getViews(statsRequestDto);
+        Map<Long, Long> eventsViewsStats = new HashMap<>();
+        for (ViewStatsDto viewStatsDto : stats) {
+            if (viewStatsDto.getUri() != null) {
+                eventsViewsStats.put(eventsWithUris.get(viewStatsDto.getUri()).getId(), viewStatsDto.getHits());
+            }
+        }
         List<ParticipationRequest> allRequestsOfEvents = requestsRepository.findParticipationRequestsByEventIn(events);
         Map<Event, Long> collect = allRequestsOfEvents.stream()
                 .collect(Collectors.groupingBy(ParticipationRequest::getEvent, Collectors.counting()));
-        return events.stream()
+        List<EventShortDto> eventShortDtos = events.stream()
                 .map(e -> EventsMapper.eventToShortDto(e, collect.get(e)))
                 .collect(Collectors.toList());
+        if (eventsViewsStats.size() > 0) {
+            for (EventShortDto eventShortDto : eventShortDtos) {
+                eventShortDto.setViews(eventsViewsStats.get(eventShortDto.getId()));
+            }
+        }
+        return eventShortDtos;
     }
 
     public EventFullDto getById(Long userId, Long eventId, HttpServletRequest httpServletRequest) {
@@ -147,8 +170,17 @@ public class PrivateEventsService {
         if (!Objects.equals(event.getInitiator().getId(), userId)) {
             throw new WrongUserException();
         }
-        return EventsMapper.eventToFullDto(event, privateRequestsService.countConfirmedRequests(
+        EventFullDto eventFullDto = EventsMapper.eventToFullDto(event, privateRequestsService.countConfirmedRequests(
                 event.getId()));
+        ArrayList<String> uris = new ArrayList<>(List.of("/events/" + eventId));
+        StatsRequestDto statsRequestDto = StatsRequestDto.builder()
+                .withStart(eventFullDto.getPublishedOn())
+                .withEnd(LocalDateTime.now())
+                .withUris(uris)
+                .withUnique(false)
+                .build();
+        eventFullDto.setViews((long) statsWebClient.getViews(statsRequestDto).size());
+        return eventFullDto;
     }
 
 }
